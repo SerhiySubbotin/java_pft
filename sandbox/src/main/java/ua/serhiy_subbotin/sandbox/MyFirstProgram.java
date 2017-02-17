f7144f9d88741ef62874209889ce8aa0cd29f391
SerhiySubbotin <serhiy_subbotin@mail.ru>
2017-02-17 16:30:06 +0200

    добавлено несколько функций

--- sandbox/src/main/java/ua/serhiy_subbotin/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/serhiy_subbotin/sandbox/MyFirstProgram.java
@@ -3,7 +3,22 @@ package ua.serhiy_subbotin.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		double l = 5;
+		double a = 4;
+		double b = 6;
+		hello("world");
+		hello("user");
+		hello("Serhiy");
+		System.out.println(l + " " + area(l));
+		System.out.println(a + " * " + b + " " + area(a, b));
 	}
-
+public static void hello(String somebody) {
+		 System.out.println("Hello, " + somebody + "!");
+}
+public static double area(double len){
+		return len * len;
+}
+public static double area(double a, double b){
+	return a*b;
+}
 }
